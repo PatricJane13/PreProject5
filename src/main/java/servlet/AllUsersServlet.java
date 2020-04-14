3b761f348e9119ec17fd08fe7f61726b5507f086
PatricJane13 <13bombom666@mail.ru>
2020-04-14 13:02:40 +0300

    Попытка №6

--- src/main/java/servlet/AllUsersServlet.java
+++ src/main/java/servlet/AllUsersServlet.java
@@ -1,7 +1,5 @@
 package servlet;
 
-import model.User;
-import org.apache.maven.lifecycle.internal.LifecycleStarter;
 import service.UserService;
 
 import javax.servlet.ServletException;
@@ -10,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/all")
 public class AllUsersServlet extends HttpServlet {
